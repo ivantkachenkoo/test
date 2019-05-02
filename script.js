4e03fb2ac431cadf07daeb88fbaecbdf93f8fa6d
Ivan Tkachenko <ivan.tkachenko0910@gmail.com>
2019-05-02 18:04:15 +0300

    Our forst commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+a = 5;
+console.log(a);
+
+// console.log(475/2)
+
+
+
+// let persone = {
+//   name: "John", 
+//   age: "25", 
+//   isMarried: false  
+// }
+
+// console.log(persone['name']);
+
+
+// //  let arr = ['plum.png','orange.jpg','apple.bnp']
+
+// //  console.log(arr[0])
+
+// //  alert('Hello World');
+
+//   let answer = +prompt("Are you 18?","Yes");
+
+//  console.log(typeof(answer));
+
+//  console.log("arr" + " - object");
+//  console.log(4 + " - object");
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let arr = ['plum.png','orange.jpg','apple.bnp']
+
+console.log(arr[0])
